aeddb07603e04cdec36f50c5da487da81536e7dd
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-22 17:54:50 +0900

    env

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+
 # pkill -f node
 sudo pm2 kill
