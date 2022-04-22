2efde178fc061a77ae8cdcce96c4cb4306c99750
LEE GUN HO (macbook13-2020) <fiadot@gmail.com>
2022-04-22 17:58:19 +0900

    12123123

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,7 +2,7 @@
 #Stopping existing node servers
 echo "Stopping any existing node servers"
 
-#add npm and node to path
+#add npm and node to path (없으면 pm2 못찾음)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
